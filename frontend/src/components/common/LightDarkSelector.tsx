d33530b0164f463354fcf58b23de6cdee3c57e4c
brendan <brenstack62@gmail.com>
2025-01-22 14:30:38 +0800

    Add system/light/dark theme functionality

--- frontend/src/components/common/LightDarkSelector.tsx
+++ frontend/src/components/common/LightDarkSelector.tsx
@@ -5,20 +5,23 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { useTheme } from "../theme-provider"
 
 /**
  * Sets the theme of the app (light/dark mode).
  */
 export default function LightDarkSelector() {
+  const { setTheme } = useTheme()
+
   return (
-    <Select>
+    <Select onValueChange={(value) => setTheme(value as "light" | "dark" | "system")}>
       <SelectTrigger className="w-[140px]">
-        <SelectValue placeholder="Theme" />
+        <SelectValue placeholder="System" />
       </SelectTrigger>
       <SelectContent>
+        <SelectItem value="system">System</SelectItem>
         <SelectItem value="light">Light</SelectItem>
         <SelectItem value="dark">Dark</SelectItem>
-        <SelectItem value="system">System</SelectItem>
       </SelectContent>
     </Select>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/Home'
+import { ThemeProvider } from './components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="dashboard" element={<Dashboard />}>
-          <Route index element={<RecentActivity />} />
-          <Route path="project/:id" element={<Project />} />
-        </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="dashboard" element={<Dashboard />}>
+            <Route index element={<RecentActivity />} />
+            <Route path="project/:id" element={<Project />} />
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
